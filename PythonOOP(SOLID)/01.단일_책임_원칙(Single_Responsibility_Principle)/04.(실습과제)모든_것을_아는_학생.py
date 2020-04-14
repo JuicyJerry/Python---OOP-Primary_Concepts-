b575c983ef7d8c4c47ed79061fe79588f0415063
JuicyJerry <ju2cyjerry@gmail.com>
2020-04-14 22:22:18 +0900

    객체 지향 프로그래밍, 견고한 객체 지향 프로그래밍(SOLID), Ch1 단일_책임_원칙(Single_Responsibility_Principle)
    실습과제

---
+++ PythonOOP(SOLID)/01.단일_책임_원칙(Single_Responsibility_Principle)/04.(실습과제)모든_것을_아는_학생.py
@@ -0,0 +1,85 @@
+"""다음은 코드잇 대학의 학생 관리 프로그램에서 사용되는 학생(Student) 클래스입니다. 학생 클래스에는
+
+학생 기본 정보 수정
+학점 추가
+평균 학점 계산
+성적표 출력"
+
+기능들이 있습니다. 그런데 학생 클래스는 너무 많은 책임을 갖고 있는 신 객체(God object)에 해당합니다.
+학생 클래스를 단일 책임 원칙에 맞게 바꿔보세요. 책임을 나누는 방식은 다양할 수 있습니다.
+일단은 여러분이 생각하는 기준으로 책임을 나눠서 과제를 풀어보세요. 그 다음 해설을 참고로 보시기 바랍니다."""
+
+
+class Student:
+    def __init__(self, name, id, major, grade):
+        self.name_book = NameBook(name)
+        self.id_card = IdentificationCard(id)
+        self.major_list = MajorList(major)
+        self.grades = GradeManager(grade)
+
+
+class NameBook:
+    def change_student_info(self, new_name):
+        """학생 기본 정보 수정 메소드"""
+        self.new_name = new_name
+
+    def print_report_card(self):
+        """학생 성적표 출력 메소드"""
+        print("코드잇 대학 성적표\n\n학생 이름:{}\n".format(self.name))
+
+class IdentificationCard:
+    def change_student_info(self, new_id):
+        """학생 기본 정보 수정 메소드"""
+        self.new_id = new_id
+
+    def print_report_card(self):
+        """학생 성적표 출력 메소드"""
+        print("학생 번호:{}\n".format(self.name_book))
+
+
+
+class MajorList:
+    def change_student_info(self, new_major):
+        """학생 기본 정보 수정 메소드"""
+        self.new_major = new_major
+
+    def print_report_card(self):
+        """학생 성적표 출력 메소드"""
+        print("소속 학과:{}\n".format(self.major_list))
+
+
+class GradeManager:
+    def __init__(self, new_grade):
+        self.grades = new_grade
+
+    def add_grade(self, new_grade):
+        """학점 추가 메소드"""
+        if 0 <= new_grade <= 4.3:
+            self.grades.append(new_grade)
+        else:
+            print("수업 학점은 0과 4.3 사이여야 합니다!")
+
+    def get_average_gpa(self):
+        """평균 학점 계산 메소드"""
+        return sum(self.grades) / len(self.grades)
+
+    def print_report_card(self):
+        """학생 성적표 출력 메소드"""
+        print("평균 학점:{}".format(self.get_average_gpa()))
+
+
+
+# 학생 인스턴스 정의
+younghoon = Student("강영훈", 20120034, "통계학과", [])
+younghoon.name_book.change_student_info("강영훈")
+younghoon.id_card.change_student_info(20130024)
+younghoon.major_list.change_student_info("컴퓨터 공학과")
+
+# 학생 성적 추가
+younghoon.grades.add_grade(3.0)
+younghoon.grades.add_grade(3.33)
+younghoon.grades.add_grade(3.67)
+younghoon.grades.add_grade(4.3)
+
+# 학생 성적표
+younghoon.grades.print_report_card()
